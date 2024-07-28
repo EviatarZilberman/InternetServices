4d12bb83e546c18ae132a9ef6c7815b75b7ffe5d
Eviatar Zilberman <esztbk@gmail.com>
2024-07-28 15:22:10 +0300

    More middleware experiments...

--- InternetServices/Program.cs
+++ InternetServices/Program.cs
@@ -6,6 +6,10 @@ var app = builder.Build();
 app.UseMiddleware<LogMiddleware>("Start Task!");
 app.UseMiddleware<LogMiddleware>("End Task!");
 
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Hello from some function in {DateTime.Now}");
+});
 
 app.MapGet("/", () => "Hello World!");
 
