b56a66fe4214f404edb0aee55c33fe61d2d66836
Eviatar Zilberman <esztbk@gmail.com>
2025-01-29 22:57:00 +0200

    Fix in the interface.

--- InternetServices/Interfaces/IHttpContextAble.cs
+++ InternetServices/Interfaces/IHttpContextAble.cs
@@ -14,10 +14,10 @@ namespace InternetServices.Interfaces
         public bool SetSessionKeyValue(string key, object value);
         public object? GetSessionValue(string key);
         public void RemoveKeyFromSession(string key);
-        public void AddSessionItem(string key, object value);
-        public object? GetSessionItem(string key);
-        public void RemoveSessionItem(string key);
-        public void ClearSessionItems();
+        public void AddRequestItem(string key, object value);
+        public object? GetRequestItem(string key);
+        public void RemoveRequestItem(string key);
+        public void ClearRequestItems();
         public IRequestCookieCollection? GetAllCookies();
         public Task<ReturnCodes> CreateCookies(DateTimeOffset? expiration, Claim[]? claims = null, bool isPersistCookie = false, string? scheme = null, string? authType = null, string claimName = ClaimsIdentity.DefaultNameClaimType, string claimRole = ClaimsIdentity.DefaultRoleClaimType);
         public string? GetCookie(string key);
